35cf6f0f445fb36c1231a29c99a0a036b29ff893
Koushik Kotha <koushik.kotha.2007@gmail.com>
2026-04-16 22:28:57 +0530

    Create C-Q4.cpp

---
+++ Assignment-9/C-Q4.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+class Arithmetic
+{
+    T a, b;
+
+public:
+    Arithmetic(T x, T y)
+    {
+        a = x;
+        b = y;
+    }
+
+    void add()
+    {
+        cout << "Addition = " << a + b << endl;
+    }
+
+    void sub()
+    {
+        cout << "Subtraction = " << a - b << endl;
+    }
+
+    void mul()
+    {
+        cout << "Multiplication = " << a * b << endl;
+    }
+
+    void div()
+    {
+        if(b != 0)
+            cout << "Division = " << a / b << endl;
+        else
+            cout << "Division not possible\n";
+    }
+};
+
+int main()
+{
+    Arithmetic<int> obj(10, 5);
+
+    obj.add();
+    obj.sub();
+    obj.mul();
+    obj.div();
+
+    return 0;
+}
