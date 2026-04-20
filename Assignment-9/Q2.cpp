ca7263578820669015e4ed33b7c61dc0f7e2dd22
Koushik Kotha <koushik.kotha.2007@gmail.com>
2026-04-20 16:05:01 +0530

    Create Q2.cpp

---
+++ Assignment-9/Q2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<fstream>
+#include<cctype>
+using namespace std;
+
+int countAlpha()
+{
+    ifstream fin("NOTES.TXT");
+    char ch;
+    int count = 0;
+
+    if(!fin)
+    {
+        cout<<"File not found";
+        return 0;
+    }
+
+    while(fin.get(ch))
+    {
+        if(isalpha(ch))
+        {
+            count++;
+        }
+    }
+
+    fin.close();
+    return count;
+}
+
+int main()
+{
+    int res = countAlpha();
+    cout<<"Total alphabets = "<<res;
+    return 0;
+}
