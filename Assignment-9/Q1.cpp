9c486c99d59ba21db81953622bd266cde0fa8673
Koushik Kotha <koushik.kotha.2007@gmail.com>
2026-04-20 16:44:49 +0530

    Created Assignment 9

---
+++ Assignment-9/Q1.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+
+    ofstream fout("NUM.TXT");
+    for(int i=1; i<=200; i++)
+    {
+        fout << i << " ";
+    }
+    fout.close();
+
+    ifstream fin("NUM.TXT");
+    ofstream f1("ODD.TXT");
+    ofstream f2("EVEN.TXT");
+
+    int num;
+
+    while(fin >> num)
+    {
+        if(num % 2 == 0)
+            f2 << num << " ";
+        else
+            f1 << num << " ";
+    }
+
+    fin.close();
+    f1.close();
+    f2.close();
+
+    int choice;
+    cout<<"Enter 1 to display Odd numbers\n";
+    cout<<"Enter 2 to display Even numbers\n";
+    cout<<"Enter choice: ";
+    cin>>choice;
+
+    ifstream show;
+
+    switch(choice)
+    {
+        case 1:
+            show.open("ODD.TXT");
+            cout<<"Odd numbers:\n";
+            break;
+
+        case 2:
+            show.open("EVEN.TXT");
+            cout<<"Even numbers:\n";
+            break;
+
+        default:
+            cout<<"Invalid choice";
+            return 0;
+    }
+
+    while(show >> num)
+    {
+        cout << num << " ";
+    }
+
+    show.close();
+
+    return 0;
+}
